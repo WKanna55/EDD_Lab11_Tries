36f90fd95856e5cef215cddfc3ac8f084a2c14e7
W Kana <willian.kana@tecsup.edu.pe>
2023-11-15 23:51:17 -0500

    tercer commit

--- Lab11_Tries_WKana.py
+++ Lab11_Tries_WKana.py
@@ -1,4 +1,4 @@
-"""Implementacion"""
+"""Ejercicio 01: Construir un trie(inicio)"""
 class NodoTrie:
     def __init__(self, valor) -> None:
         self.valor = valor
@@ -44,7 +44,9 @@ class Trie:
         prefijo = prefijo + "|  "
         for char in nodo.hijos:
             self.imprimir_trie_(nodo.hijos[char], prefijo)
+    """Ejercicio 01: Construir un trie(fin)"""
 
+    """Ejercicio 02: Busqueda un trie(inicio)"""
     def buscar(self, palabra):
         actual = self.raiz
         for char in palabra:
@@ -52,7 +54,31 @@ class Trie:
                 return False
             actual = actual.hijos[char]
         return actual.eow
+    """Ejercicio 02: Busqueda un trie(fin)"""
 
+    """Ejercicio 03: Contar palabras con cierto prefijo(inicio)"""
+    def contar_palabras(self):
+        return self.contar_palabras_(self.raiz)
+
+    # Time Complexity: O(n) n is the number of characters
+    # Space Complexity: O(m*L)
+    # --> m is the number of words, L length of each word
+    def contar_palabras_(self, nodo):
+        contador = 0
+        if nodo.eow: contador += 1
+        for item in nodo.hijos.values():  # times m
+            contador += self.contar_palabras_(item)  # length of word/item
+        return contador
+    def contar_palabra_prefijo(self, prefijo):
+        actual = self.raiz
+        for char in prefijo:
+            if char not in actual.hijos:
+                return False
+            actual = actual.hijos[char]
+        return self.contar_palabras_(actual)
+    """Ejercicio 03: Contar palabras con cierto prefijo(fin)"""
+
+    """Ejercicio 04: Eliminacion de palabras del trie(inicio)"""
     def eliminar(self, palabra):
         if palabra is None: return
         self.eliminar_(self.raiz, palabra, 0)
@@ -66,36 +92,73 @@ class Trie:
         self.eliminar_(hijo, palabra, indice + 1)
         if len(hijo.hijos) == 0 and not hijo.eow:
             nodo.hijos.pop(char)
+    """Ejercicio 04: Eliminacion de palabras del trie(fin)"""
 
-    def contar_palabras(self):
-        return self.contar_palabras_(self.raiz)
+    """Ejercicio 05: Autocompletado(inicio)"""
+    def completar(self, nodo, prefijo, resultado_array):
+        if nodo.eow: resultado_array.append(prefijo)
+        for char, hijo in nodo.hijos.items():
+            self.completar(hijo, prefijo + char, resultado_array)
+    def autocompletar(self, cadena):
+        actual = self.raiz
+        resultado = []
+        for char in cadena:
+            if char not in actual.hijos:
+                return False
+            actual = actual.hijos[char]
+        self.completar(actual, cadena, resultado)
+        return resultado
+    """Ejercicio 05: Autocompletado(fin)"""
 
-    # Time Complexity: O(n) n is the number of characters
-    # Space Complexity: O(m*L)
-    # --> m is the number of words, L length of each word
-    def contar_palabras_(self, nodo):
-        contador = 0
-        if nodo.eow: contador += 1
-        for item in nodo.hijos.values():  # times m
-            contador += self.contar_palabras_(item)  # length of word/item
-        return contador
+    """Ejercicio 06: Palabras unicas en un texto(inicio)"""
+    def palabras_unicas(self, texto_largo):
+        translator = str.maketrans('0123456789,.', ' ' * len('0123456789,.'))
+        cadena_con_espacios = texto_largo.translate(translator)
+        palabras = cadena_con_espacios.split()
+
+        for palabra in palabras:
+            self.insertar(palabra)
+
+        return self.contar_palabras()
+    """Ejercicio 06: Palabras unicas en un texto(fin)"""
+
+    """Ejercicio 07: Longitud de la palabra más larga(inicio)"""
+    def palabra_larga(self, nodo):
+        if not nodo.hijos:
+            return 0
+
+        longitud = [self.palabra_larga(hijo) for hijo in nodo.hijos.values()]
+        return 1 + max(longitud, default=0)
+
+    """Ejercicio 07: Longitud de la palabra más larga(fin)"""
+
 
 
 trie = Trie()
 trie.insertar("INTERACT")
 trie.insertar("INTERNATIONAL")
 trie.insertar("INTERNET")
-print(trie.buscar("INTERACT"))
-print(trie.buscar("INTERN"))
+trie.insertar("AND")
+trie.insertar("ANT")
+trie.insertar("ANTHEM")
+#print(trie.buscar("INTERACT"))
+#print(trie.buscar("INTERN"))
 #trie.delete("INTERNATIONAL")
 #trie.pre_order()
-#trie.print()
-trie.post_order()
-print(trie.contar_palabras())
+#trie.imprimir_trie()
+#trie.post_order()
+#print(trie.contar_palabras())
+#print(trie.contar_palabra_prefijo("ANT"))
+cadena_input = "INTERN"
+#print(trie.autocompletar(cadena_input))
 
+#texto con 159 palabras
+texto = """En un pequeño pueblo llamado Aldeavieja, vivía un anciano llamado Juan. Juan tenía una granja con muchos animales. Por las mañanas, Juan solía dar de comer a sus gallinas y ovejas. Las gallinas cacareaban y las ovejas balaban, creando un ambiente animado.
+Un día, Juan decidió plantar un jardín. Plantó flores de colores brillantes, como rosas y lirios. También sembró vegetales como tomates, zanahorias y lechugas. El jardín de Juan se convirtió en un lugar hermoso y lleno de vida.
+Los vecinos de Aldeavieja a menudo visitaban a Juan para disfrutar de su jardín y charlar con él. Compartían historias y risas, creando una comunidad unida. A veces, organizaban pequeñas fiestas en la granja de Juan, donde todos compartían comida y alegría.
+A medida que pasaba el tiempo, las estaciones cambiaban, pero la amistad en Aldeavieja permanecía fuerte. Juan continuaba cuidando de sus animales y su jardín, y la gente seguía visitándolo para compartir momentos felices. La vida en Aldeavieja era simple pero llena de significado y conexión."""
 
-"""Ejercicio 01: Construir un trie(inicio)"""
-"""Ejercicio 01: Construir un trie(fin)"""
+trie2 = Trie()
+#print(trie2.palabras_unicas(texto))
 
-"""Ejercicio 01: Construir un trie(inicio)"""
-"""Ejercicio 01: Construir un trie(inicio)"""
+#print(trie.palabra_larga(trie.raiz))
